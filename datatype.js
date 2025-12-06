c5df6f7e7b64735ff6f8da1f0e72f90208efa1be
Antonyjoshua <antonyjoshua413@gmail.com>
2025-12-06 18:08:33 +0530

    First commit

---
+++ datatype.js
@@ -0,0 +1,9 @@
+var browserversion=1.1;
+var browsername="Chrome";
+var isbrowseropen=true;
+var history=null;
+var profile;
+console.log("Browser Versiom:" +browserversion + "\n"+"Browser Name:" +browsername+ "\n"+"Browser status:" +isbrowseropen+
+    "\n"+"History:"+history+"\n"+ "Profile datatype:" +profile);
+console.log("History datatype"+typeof history +"\n"+"Profile datatype:" +typeof profile);
+
---
+++ variableDeclaration.js
@@ -0,0 +1,31 @@
+
+//var
+// var username="test@yopmail.com";
+// console.log("Username dataype:" +typeof username)
+// username=true;
+// console.log("Username dataype:" +typeof username)
+
+//variable reassignment
+var num=45;
+num="test@yopmail.com";
+console.log("num dataype:" +typeof num)
+console.log("num value:" +num)
+//variable redeclaration
+var num=true;
+console.log("num dataype:" +typeof num)
+console.log("num value:" +num)
+
+//let reassignment
+let empname="testing";
+empname=123;
+console.log("Empname datatype="+ empname)
+console.log("Empname value="+ empname)
+//let redeclaration
+//let empname=true;
+
+//const reassignment
+const final=33;
+final=true;
+console.log("final="+final)
+//const redeclaration
+//const final=4;
